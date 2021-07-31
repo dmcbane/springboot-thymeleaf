013cb109a9c1e68003429c93e0aaaa7a8ecf9ac3
H. Dale McBane <h.dale.mcbane@gmail.com>
2021-07-31 18:50:49 -0400

    Display better error messages for routing errors
    ..also used Bootstrap to improve the look of the normal page while I was
    there.

--- src/main/java/com/example/springbootdocker/controller/HelloController.java
+++ src/main/java/com/example/springbootdocker/controller/HelloController.java
@@ -6,32 +6,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Objects;
+
 @Controller
 public class HelloController {
 
 	@GetMapping({"/", "/hello", "/hello/"})
-	public String hello(Model model, @RequestParam(value="reqName", required=false, defaultValue="World") String reqName) {
+	public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
 		model.addAttribute("name", "Only RequestParam");
-		model.addAttribute("reqName", reqName);
+		model.addAttribute("pathName", "Not used");
+		model.addAttribute("reqName", name);
 		return "hello";
 	}
 
-	@GetMapping({"/{pathName}", "/hello/{pathName}"})
-	public String hello(Model model, @RequestParam(value="reqName", required=false, defaultValue="World") String reqName, @PathVariable(required = false) String pathName) {
-		model.addAttribute("name", "both");
-		model.addAttribute("pathName", pathName);
-		model.addAttribute("reqName", reqName);
-		return "hello";
-	}
-
-	@GetMapping({"/greetings/{pathName}"})
+	@GetMapping({"/greetings", "/greetings/{pathName}"})
 	public String greetings(Model model, @PathVariable(required = false) String pathName) {
 		model.addAttribute("name", "Only PathVariable");
-		if (pathName == null) {
-			model.addAttribute("pathName", "null");
-		} else {
-			model.addAttribute("pathName", pathName);
-		}
+		model.addAttribute("pathName", Objects.requireNonNullElse(pathName, "Not set"));
+		model.addAttribute("reqName", "Not used");
+		return "hello";
+	}
+
+	@GetMapping({"/welcome{pathName}", "/welcome/{pathName}"})
+	public String welcome(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name, @PathVariable(required = false) String pathName) {
+		model.addAttribute("name", "PathVariable & RequestParam");
+		model.addAttribute("pathName", Objects.requireNonNullElse(pathName, "Not set"));
+		model.addAttribute("reqName", name);
 		return "hello";
 	}
 
