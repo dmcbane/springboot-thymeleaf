fb8db59264a74cbc52459e18893b32a26d2cac59
H. Dale McBane <h.dale.mcbane@gmail.com>
2021-07-31 12:25:38 -0400

    pathvariables and requestparameters

--- src/main/java/com/example/springbootdocker/controller/HelloController.java
+++ src/main/java/com/example/springbootdocker/controller/HelloController.java
@@ -3,14 +3,23 @@ package com.example.springbootdocker.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
-	@GetMapping({"/", "/hello"})
+	@GetMapping({"/", "/hello", "/hello/"})
 	public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
 		model.addAttribute("name", name);
+		return "hello";
+	}
+
+	@GetMapping({"/{name}", "/hello/{name}"})
+	public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String reqName, @PathVariable(required = false) String pathName) {
+		model.addAttribute("name", "both");
+		model.addAttribute("pathName", pathName);
+		model.addAttribute("reqname", reqName);
 		return "hello";
 	}
 
